2ff603e3436ad2b366c7b9dc1d5e9d270a652299
youngs-cyeww <discorddiscord1214@gmail.com>
2025-11-16 18:28:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 # config.py
-DB_CONFIG = {
+
+MYSQL_CONFIG = {
     "host": "localhost",
-    "user": "flask_user",
+    "user": "root",
     "password": "12345678",
     "database": "logistics"
 }
+
+# 你的倉庫地圖大小
+MAP_WIDTH = 20
+MAP_HEIGHT = 20
+
+# 固定障礙（如果沒有可以留空）
+STATIC_OBSTACLES = [
+    (5, 5), (5, 6), (5, 7)
+]
