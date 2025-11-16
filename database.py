98486e644aa3fbc12e873f90bdcc996703d7cddc
youngs-cyeww <discorddiscord1214@gmail.com>
2025-11-16 18:28:19 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,26 @@
 # database.py
-import mysql.connector
-from config import DB_CONFIG
 
-def get_db():
-    return mysql.connector.connect(**DB_CONFIG)
+import mysql.connector
+from config import MYSQL_CONFIG
+
+db = mysql.connector.connect(**MYSQL_CONFIG)
+cursor = db.cursor(dictionary=True)
+
+def update_vehicle(vehicle_id, x, y, battery, status):
+    sql = """
+    REPLACE INTO vehicles (id, x, y, battery, status)
+    VALUES (%s, %s, %s, %s, %s)
+    """
+    cursor.execute(sql, (vehicle_id, x, y, battery, status))
+    db.commit()
+
+def get_other_vehicles(vehicle_id):
+    cursor.execute("SELECT id, x, y FROM vehicles WHERE id != %s", (vehicle_id,))
+    return cursor.fetchall()
+
+def get_vehicle_task(vehicle_id):
+    cursor.execute(
+        "SELECT * FROM tasks WHERE vehicle_id = %s AND done = 0 LIMIT 1",
+        (vehicle_id,)
+    )
+    return cursor.fetchone()
